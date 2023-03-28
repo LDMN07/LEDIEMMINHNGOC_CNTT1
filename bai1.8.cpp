242cf7f225916b8ddd57f1e640b17b1649b77b20
LÊ DIỄM MINH NGỌC <125197810+LDMN07@users.noreply.github.com>
2023-03-28 13:54:07 +0700

    Create bai1.8.cpp

---
+++ bai1.8.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+struct duongThang {
+    float a;
+    float b;
+    float c;
+}
+void nhapDuongThang(struct duongThang *dt) {
+    printf("Nhap he so a: ");
+    scanf("%f",(dt->a));
+    printf("Nhap he so b: ");
+    scanf("%f",(dt->b));
+    printf("Nhap he so c: ");
+    scanf("%f",(dt->c));
+}
+void xuatDuongThang(struct duongThang dt) {
+    printf("%.2f*x + %.2f*y + %.2f = 0", dt.a, dt.b, dt.c);
+}
+ int main()
+ {
+        duongthang(dt);
+ 	nhapduongthang(td);
+ 	xuatduongthang(td);
+ 	return 0;
+ }
